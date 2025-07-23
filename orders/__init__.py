80c082adc80afda17d9d375bc2c2f1fbcef66c19
Zander Peterson <zander.j.peterson@gmail.com>
2025-07-23 14:18:26 +1200

    Structured Order Package

---
+++ orders/__init__.py
@@ -0,0 +1,4 @@
+# When the "orders" package is imported through main.py, this file will
+# ensure that the classes, variables, etc., will be correctly imported.
+
+from order import Order
---
+++ orders/order.py
@@ -0,0 +1,8 @@
+# Contains the "Order" class, with every instance tracking a single order.
+
+class Order():
+    """
+    A Class which will have one instance per order, and tracks various
+    order-specific bits of information, such as start/end dates, customer
+    name, receipt number, the item, the quantity, raffle info, etc. etc.
+    """
