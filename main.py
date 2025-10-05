80198c01b7cd0e7bb441760654985ee203534c6c
Zander Peterson <zander.j.peterson@gmail.com>
2025-10-06 00:55:15 +1300

    Added Basic Window
    Added a window to the project. It has a title of "Test" and is currently blank.

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 # This is the file that the user should execute.
 
-from orders import Order
+import sys
 from datetime import datetime
+
+from PySide6.QtWidgets import QApplication
+
+from widgets import Widget
+from orders import Order
+
+app = QApplication(sys.argv)
+
+widget = Widget()
+widget.show()
+
+app.exec()
 
 # The below lines are for testing purposes.
 order_test = Order(777,
