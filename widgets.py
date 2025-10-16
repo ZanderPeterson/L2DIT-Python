cd34a60c07b8d24e7d8408ce6f4ee29e16f46db3
Zander Peterson <zander.j.peterson@gmail.com>
2025-10-17 09:53:59 +1300

    Added Create New Order Label

--- widgets.py
+++ widgets.py
@@ -1,7 +1,14 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Test")
+        self.setWindowTitle("Party Hire Store — Order Tracker")
+
+        input_order_section = QVBoxLayout()
+
+        add_order_label = QLabel("<h1>Create New Order</h1>")
+        input_order_section.addWidget(add_order_label)
+
+        self.setLayout(input_order_section)
