b90a6af775ceed80f12aaca5d134650cd92aa22d
Zander Peterson <zander.j.peterson@gmail.com>
2025-10-17 16:43:27 +1300

    Added Receipt Number Input

--- widgets.py
+++ widgets.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton
+from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QPushButton, QTextEdit
 
 class Widget(QWidget):
     def __init__(self):
@@ -10,5 +10,17 @@ class Widget(QWidget):
 
         add_order_label = QLabel("<h1>Create New Order</h1>")
         input_order_section.addWidget(add_order_label)
+
+
+        receipt_number_layout = QHBoxLayout()
+
+        add_order_label = QLabel("Receipt Number:")
+        receipt_number_layout.addWidget(add_order_label)
+
+        receipt_number_input = QTextEdit()
+        receipt_number_input.setFixedSize(200, 20)
+        receipt_number_layout.addWidget(receipt_number_input)
+
+        input_order_section.addLayout(receipt_number_layout)
 
         self.setLayout(input_order_section)
