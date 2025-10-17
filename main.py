05adddab082ed1998910443dd51b1a2f44652d68
Zander Peterson <zander.j.peterson@gmail.com>
2025-10-17 23:32:30 +1300

    Cleaned Up main.py
    Removed old lines that were used for testing purposes

--- main.py
+++ main.py
@@ -14,16 +14,3 @@ widget = Widget()
 widget.show()
 
 app.exec()
-
-# The below lines are for testing purposes.
-order_test = Order(777,
-                   "Jaz",
-                   "drumsticks",
-                   123,
-                   datetime(2025, 9, 10),
-                   datetime(2025, 10, 10))
-print(order_test.get_boxes_required())
-print(f"Number of boxes required: {order_test.get_boxes_required(126)}")
-print(f"Order Duration: {order_test.get_order_duration_days()} day(s)")
-print(f"Raffle Number: {order_test.raffle_number}")
-order_test.print_order()
