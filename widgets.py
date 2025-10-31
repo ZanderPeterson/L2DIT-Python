305b32aedc8d0089e3c28d9db722b95bf72b5ea9
Zander Peterson <zander.j.peterson@gmail.com>
2025-10-31 14:52:43 +1300

    Added More Comments to Widget Class

--- widgets.py
+++ widgets.py
@@ -7,162 +7,162 @@ class Widget(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Party Hire Store — Order Tracker")
+        self.setWindowTitle("Party Hire Store — Order Tracker") #Sets the title of the window
 
-        widget_layout = QVBoxLayout()
+        widget_layout = QVBoxLayout() #Defines the overall layout, which is vertical
 
-        input_order_section = QVBoxLayout()
+        input_order_section = QVBoxLayout() #Defines the layout for the input order section, with is vertical
 
-        add_order_label = QLabel("<h1>Create New Order</h1>")
-        input_order_section.addWidget(add_order_label)
+        add_order_label = QLabel("<h1>Create New Order</h1>") #Adds in the New Order Heading
+        input_order_section.addWidget(add_order_label) #Adds heading to input order section layout
 
         #Receipt Number Input
-        receipt_number_layout = QHBoxLayout()
+        receipt_number_layout = QHBoxLayout() #Defines a layout for the receipt number portion, which is horizontal
 
-        add_order_label = QLabel("Receipt Number:")
-        receipt_number_layout.addWidget(add_order_label)
+        add_order_label = QLabel("Receipt Number:") #Adds a label indicating the proceeding field is for the Receipt Number
+        receipt_number_layout.addWidget(add_order_label) #Adds this label to the receipt number layout
 
-        self.receipt_number_input = QTextEdit()
-        self.receipt_number_input.setFixedSize(200, 28)
-        receipt_number_layout.addWidget(self.receipt_number_input)
+        self.receipt_number_input = QTextEdit() #Creates a text box
+        self.receipt_number_input.setFixedSize(200, 28) #Forces the text box to adhere to a specific size
+        receipt_number_layout.addWidget(self.receipt_number_input) #Adds the text box to the receipt number layout
 
-        input_order_section.addLayout(receipt_number_layout)
+        input_order_section.addLayout(receipt_number_layout) #Adds the receipt number layout to the overall input section layout
 
         #Order Name Input
-        order_name_layout = QHBoxLayout()
+        order_name_layout = QHBoxLayout() #Defines a layout for the Customer Name portion, which is horizontal
 
-        add_name_label = QLabel("Name:")
-        order_name_layout.addWidget(add_name_label)
+        add_name_label = QLabel("Name:") #Adds a label prompting the user to input the customer's name
+        order_name_layout.addWidget(add_name_label) #Adds this label to order name layout
 
-        self.order_name_input = QTextEdit()
-        self.order_name_input.setFixedSize(200, 28)
-        order_name_layout.addWidget(self.order_name_input)
+        self.order_name_input = QTextEdit() #Creates a text box
+        self.order_name_input.setFixedSize(200, 28) #Forces the text box to be of a specific size
+        order_name_layout.addWidget(self.order_name_input) #Adds this text box to the layout
 
-        input_order_section.addLayout(order_name_layout)
+        input_order_section.addLayout(order_name_layout) #Adds the customer name layout to the overall input section layout
 
         #Item Hired
-        item_hired_layout = QHBoxLayout()
+        item_hired_layout = QHBoxLayout() #Defines a layout for the Item Hired portion, which is horizontal
 
-        item_hired_label = QLabel("Item Hired:")
-        item_hired_layout.addWidget(item_hired_label)
+        item_hired_label = QLabel("Item Hired:") #Adds a label prompting the user to input the Item Hired
+        item_hired_layout.addWidget(item_hired_label) #Adds this label to item hired layout
 
-        self.item_hired_input = QTextEdit()
-        self.item_hired_input.setFixedSize(200, 28)
-        item_hired_layout.addWidget(self.item_hired_input)
+        self.item_hired_input = QTextEdit() #Creates a text box
+        self.item_hired_input.setFixedSize(200, 28) #Forces the text box to be of a specific size
+        item_hired_layout.addWidget(self.item_hired_input) #Adds this text box to the layout
 
-        input_order_section.addLayout(item_hired_layout)
+        input_order_section.addLayout(item_hired_layout) #Adds this layout to the overall input section layout
 
         #Item Quantity
-        item_quantity_layout = QHBoxLayout()
+        item_quantity_layout = QHBoxLayout() #Defines a layout for the Item Quantity portion, which is horizontal
 
-        item_quantity_label = QLabel("Item Quantity:")
-        item_quantity_layout.addWidget(item_quantity_label)
+        item_quantity_label = QLabel("Item Quantity:") #Adds a label prompting the user to input the Item Quantity
+        item_quantity_layout.addWidget(item_quantity_label) #Adds this label to item quantity layout
 
-        self.item_quantity_input = QTextEdit()
-        self.item_quantity_input.setFixedSize(200, 28)
-        item_quantity_layout.addWidget(self.item_quantity_input)
+        self.item_quantity_input = QTextEdit() #Creates a text box
+        self.item_quantity_input.setFixedSize(200, 28) #Forces the text box to be of a specific size
+        item_quantity_layout.addWidget(self.item_quantity_input) #Adds this text box to the layout
 
-        input_order_section.addLayout(item_quantity_layout)
+        input_order_section.addLayout(item_quantity_layout) #Adds this layout to the overall input section layout
 
         #Order Start Date
-        order_start_date_layout = QHBoxLayout()
+        order_start_date_layout = QHBoxLayout() #Defines a layout for the Start Date portion, which is horizontal
 
-        order_start_date_label = QLabel("Item Start Date [DD/MM/YYYY]: ")
-        order_start_date_layout.addWidget(order_start_date_label)
+        order_start_date_label = QLabel("Item Start Date [DD/MM/YYYY]: ") #Adds a label prompting the user to input the Start Date, and explains how to input
+        order_start_date_layout.addWidget(order_start_date_label) #Adds this label to layout
 
-        self.item_start_date_input_days = QTextEdit()
-        self.item_start_date_input_days.setFixedSize(50, 28)
-        order_start_date_layout.addWidget(self.item_start_date_input_days)
+        self.item_start_date_input_days = QTextEdit() #Creates a text box
+        self.item_start_date_input_days.setFixedSize(50, 28) #Forces the text box to be of a specific size — this is smaller than most other fields
+        order_start_date_layout.addWidget(self.item_start_date_input_days) #Adds this text book to the layout
 
-        self.item_start_date_input_months = QTextEdit()
-        self.item_start_date_input_months.setFixedSize(50, 28)
-        order_start_date_layout.addWidget(self.item_start_date_input_months)
+        self.item_start_date_input_months = QTextEdit() #Creates a text box
+        self.item_start_date_input_months.setFixedSize(50, 28) #Forces the text box to be of a specific size — this is smaller than most other fields
+        order_start_date_layout.addWidget(self.item_start_date_input_months) #Adds this text book to the layout
 
-        self.item_start_date_input_years = QTextEdit()
-        self.item_start_date_input_years.setFixedSize(50, 28)
-        order_start_date_layout.addWidget(self.item_start_date_input_years)
+        self.item_start_date_input_years = QTextEdit() #Creates a text box
+        self.item_start_date_input_years.setFixedSize(50, 28) #Forces the text box to be of a specific size — this is smaller than most other fields
+        order_start_date_layout.addWidget(self.item_start_date_input_years) #Adds this text book to the layout
 
-        input_order_section.addLayout(order_start_date_layout)
+        input_order_section.addLayout(order_start_date_layout) #Adds this layout to the overall input section layout
 
         #Order End Date
-        order_end_date_layout = QHBoxLayout()
+        order_end_date_layout = QHBoxLayout() #Defines a layout for the End Date portion, which is horizontal
 
-        order_end_date_label = QLabel("Item End Date [DD/MM/YYYY]: ")
-        order_end_date_layout.addWidget(order_end_date_label)
+        order_end_date_label = QLabel("Item End Date [DD/MM/YYYY]: ") #Adds a label prompting the user to input the End Date, and explains how to input
+        order_end_date_layout.addWidget(order_end_date_label) #Adds this label to layout
 
-        self.item_end_date_input_days = QTextEdit()
-        self.item_end_date_input_days.setFixedSize(50, 28)
-        order_end_date_layout.addWidget(self.item_end_date_input_days)
+        self.item_end_date_input_days = QTextEdit() #Creates a text box
+        self.item_end_date_input_days.setFixedSize(50, 28) #Forces the text box to be of a specific size — this is smaller than most other fields
+        order_end_date_layout.addWidget(self.item_end_date_input_days) #Adds this text book to the layout
 
-        self.item_end_date_input_months = QTextEdit()
-        self.item_end_date_input_months.setFixedSize(50, 28)
-        order_end_date_layout.addWidget(self.item_end_date_input_months)
+        self.item_end_date_input_months = QTextEdit() #Creates a text box
+        self.item_end_date_input_months.setFixedSize(50, 28) #Forces the text box to be of a specific size — this is smaller than most other fields
+        order_end_date_layout.addWidget(self.item_end_date_input_months) #Adds this text book to the layout
 
-        self.item_end_date_input_years = QTextEdit()
-        self.item_end_date_input_years.setFixedSize(50, 28)
-        order_end_date_layout.addWidget(self.item_end_date_input_years)
+        self.item_end_date_input_years = QTextEdit() #Creates a text box
+        self.item_end_date_input_years.setFixedSize(50, 28) #Forces the text box to be of a specific size — this is smaller than most other fields
+        order_end_date_layout.addWidget(self.item_end_date_input_years) #Adds this text book to the layout
 
-        input_order_section.addLayout(order_end_date_layout)
+        input_order_section.addLayout(order_end_date_layout) #Adds this layout to the overall input section layout
 
-        submit_order_selection_button = QPushButton("Submit")
-        input_order_section.addWidget(submit_order_selection_button)
-        submit_order_selection_button.clicked.connect(self.submit_order)
+        submit_order_selection_button = QPushButton("Submit") #Creates a button
+        input_order_section.addWidget(submit_order_selection_button) #Adds the button to the overall input layout
+        submit_order_selection_button.clicked.connect(self.submit_order) #Connections the button clicked event to the function "self.submit_order"
 
-        widget_layout.addLayout(input_order_section)
+        widget_layout.addLayout(input_order_section) #Adds the input section layout to the overall window layout
 
 
         #Output
-        output_order_section = QVBoxLayout()
+        output_order_section = QVBoxLayout() #Creates the overall layout for the output section
 
-        add_order_label = QLabel("<h1>Get Order(s)</h1>")
-        input_order_section.addWidget(add_order_label)
+        add_order_label = QLabel("<h1>Get Order(s)</h1>") #Creates a heading for the output section
+        input_order_section.addWidget(add_order_label) #Adds that heading to the layout
 
-        print_all_button = QPushButton("Show All Orders")
-        output_order_section.addWidget(print_all_button)
-        print_all_button.clicked.connect(self.print_all)
+        print_all_button = QPushButton("Show All Orders") #Creates a button
+        output_order_section.addWidget(print_all_button) #Adds button to the layout
+        print_all_button.clicked.connect(self.print_all) #Connects the button with the "self.print_all" function
 
         #Search Tool
-        search_by_receipt_layout = QHBoxLayout()
+        search_by_receipt_layout = QHBoxLayout() #Defines the layout for the search by receipt section
 
-        search_by_receipt_label = QLabel("Search By Receipt:")
-        search_by_receipt_layout.addWidget(search_by_receipt_label)
+        search_by_receipt_label = QLabel("Search By Receipt:") #Creates a relevant label
+        search_by_receipt_layout.addWidget(search_by_receipt_label) #Adds the label to the layout
 
-        self.search_by_receipt_input = QTextEdit()
-        self.search_by_receipt_input.setFixedSize(180, 28)
-        search_by_receipt_layout.addWidget(self.search_by_receipt_input)
+        self.search_by_receipt_input = QTextEdit() #Creates a text box
+        self.search_by_receipt_input.setFixedSize(180, 28) #Forces the text box to be of a specific size
+        search_by_receipt_layout.addWidget(self.search_by_receipt_input) #Adds the text box to the layout
 
-        search_by_receipt_button = QPushButton("Search")
-        search_by_receipt_layout.addWidget(search_by_receipt_button)
-        search_by_receipt_button.clicked.connect(self.search_by_receipt_function)
+        search_by_receipt_button = QPushButton("Search") #Creates a button
+        search_by_receipt_layout.addWidget(search_by_receipt_button) #Adds button to the layout
+        search_by_receipt_button.clicked.connect(self.search_by_receipt_function) #Connects the button with the "self.search_by_receipt_function" function
 
-        search_by_receipt_delete_button = QPushButton("Del")
-        search_by_receipt_layout.addWidget(search_by_receipt_delete_button)
-        search_by_receipt_delete_button.clicked.connect(self.search_by_receipt_delete_function)
+        search_by_receipt_delete_button = QPushButton("Del") #Creates a button
+        search_by_receipt_layout.addWidget(search_by_receipt_delete_button) #Adds button to the layout
+        search_by_receipt_delete_button.clicked.connect(self.search_by_receipt_delete_function) #Connects the button with the "self.search_by_receipt_delete_function" function
 
-        output_order_section.addLayout(search_by_receipt_layout)
+        output_order_section.addLayout(search_by_receipt_layout) #Adds this layout to the overall output section layout
 
         #Search for Raffle Winner Tool
-        search_by_raffle_layout = QHBoxLayout()
+        search_by_raffle_layout = QHBoxLayout() #Defines the layout for the search by raffle section
 
-        search_by_raffle_label = QLabel("Search For Raffle:")
-        search_by_raffle_layout.addWidget(search_by_raffle_label)
+        search_by_raffle_label = QLabel("Search For Raffle:") #Creates a relevant label
+        search_by_raffle_layout.addWidget(search_by_raffle_label) #Adds the label to the layout
 
-        self.search_by_raffle_input = QTextEdit()
-        self.search_by_raffle_input.setFixedSize(180, 28)
-        search_by_raffle_layout.addWidget(self.search_by_raffle_input)
+        self.search_by_raffle_input = QTextEdit() #Creates a text box
+        self.search_by_raffle_input.setFixedSize(180, 28) #Forces the text box to be of a specific size
+        search_by_raffle_layout.addWidget(self.search_by_raffle_input) #Adds the text box to the layout
 
-        search_by_raffle_button = QPushButton("Search")
-        search_by_raffle_layout.addWidget(search_by_raffle_button)
-        search_by_raffle_button.clicked.connect(self.search_by_raffle_function)
+        search_by_raffle_button = QPushButton("Search") #Creates a button
+        search_by_raffle_layout.addWidget(search_by_raffle_button) #Adds button to the layout
+        search_by_raffle_button.clicked.connect(self.search_by_raffle_function) #Connects the button with the "self.search_by_raffle_function" function
 
-        output_order_section.addLayout(search_by_raffle_layout)
+        output_order_section.addLayout(search_by_raffle_layout) #Adds this layout to the overall output section layout
 
 
-        widget_layout.addLayout(output_order_section)
+        widget_layout.addLayout(output_order_section) #Adds the output layout section to the overall window layout
 
-        self.setLayout(widget_layout)
+        self.setLayout(widget_layout) #Sets the window layout to be the window layout
 
-        self.orders = []
+        self.orders = [] #Creates an empty list where instances of the Order class will be stored
 
     def submit_order(self) -> None:
         """
